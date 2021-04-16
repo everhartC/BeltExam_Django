18615be50778a10ff6c18d3b828694fcfe6a93e9
Adam Joines <amountcastlej@gmail.com>
2021-04-15 21:35:34 -0400

    Added login and reg for urls path, methods, templates and user models

--- BeltExam_Project/beltApp/models.py
+++ BeltExam_Project/beltApp/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+import re
+import bcrypt
+class UserManager(models.Manager): 
+    def Registration_Validator(self, postData): 
+        errors = {} 
+        if len(postData['name']) < 2: 
+            errors['name'] = " name must be 2 characters or more" 
+            if len(postData['alias']) < 2: 
+                errors['alisa'] = " Alias name must be 2 characters or more" 
+                user_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+                if not user_regex.match(postData['email']): 
+                    errors['email'] = "You must enter in a valid email" 
+                    check_email = User.objects.filter(email=postData['email']) 
+                    if len(check_email) > 0: 
+                        errors['emailExists'] = "Already a user with this email in our database" 
+                    if len(postData['password']) < 8: 
+                        errors['password'] = "Password must be at least 8 characters long" 
+                    if postData['password'] != postData['passwordC']: 
+                        errors['passwordC'] = "Your password and confirm password do not match" 
+        return errors
 
-# Create your models here.
+    def Login_Validator(self, postData): 
+        errors = {} 
+        login_user = User.objects.filter(email=postData['email']) 
+        if len(login_user) > 0: 
+            if bcrypt.checkpw(postData['password'].encode(), login_user[0].password.encode()): 
+                print("Password matches") 
+            else: errors['password'] = "Password does not match!" 
+        else: 
+            errors['email'] = "There is no user with that email" 
+        return errors
+
+class User(models.Model): 
+    name=models.CharField(max_length=30) 
+    alias=models.CharField(max_length=30) 
+    email = models.CharField(max_length=80) 
+    password = models.CharField(max_length=32) 
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True) 
+    objects = UserManager()
--- BeltExam_Project/beltApp/views.py
+++ BeltExam_Project/beltApp/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import *
+import bcrypt
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Hello")
+    request.session.flush()
+    return render(request, 'index.html')
+
+def register(request): 
+    if request.method == "POST": 
+        errors = User.objects.Registration_Validator(request.POST) 
+        if len(errors) != 0: 
+            for key, value in errors.items(): 
+                messages.error(request, value) 
+            return redirect('/') 
+        else: 
+            hashed_pw = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode() 
+            new_user = User.objects.create(name=request.POST['name'], alias=request.POST['alias'], email = request.POST['email'], password = hashed_pw) 
+            request.session['user_id'] = new_user.id 
+            return redirect('/books')  
+    return redirect('/')
+
+def login(request): 
+    if request.method == "POST": 
+        errors = User.objects.Login_Validator(request.POST) 
+    if len(errors) > 0: 
+        for key, value in errors.items(): 
+            messages.error(request, value) 
+    return redirect('/') 
+    this_user = User.objects.filter(email = request.POST['email']) 
+    request.session['user_id'] = this_user[0].id 
+    return redirect('/books') 
+    return redirect('/')
+
+def logout(request): 
+    request.session.flush() 
+    return redirect('/')
